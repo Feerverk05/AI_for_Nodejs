399f1824b87759c36d1fdf30d84baa99f11ef314
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-20 14:14:39 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import { OpenAi } from 'openai'
-
-const openai = new OpenAi()
+import { OpenAI } from 'openai'
+import { encoding_for_model } from 'tiktoken'
+const openai = new OpenAI()
 
 async function main(){
-    const response = await openai.chat.compleions.create({
+    const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages:[{
             role: 'user',
-            content: 'How tall is mount everest?'
+            content: 'How tall is mount Everest'
+        },{
+            role: 'user',
+            content: 'How tall is mount Everest?'
         }]
     })
-    console.log(response.choice[0].message.context)
+    console.log(response.choices[0].message.content)
 }
-main();
+
+function encodePrompt(){
+    const prompt = "How are you today?"
+    const encoder = encoding_for_model('gpt-3.5-turbo');
+    const words = encoder.encode(prompt);
+    console.log(words)
+}
+
+encodePrompt();
