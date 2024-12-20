e8fce0230cd35810780abf7d5c289d63958a4d57
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-20 14:59:08 +0200

    Update index.ts

--- chat_app/src/basic_chat/index.ts
+++ chat_app/src/basic_chat/index.ts
@@ -1,18 +1,28 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI();
+const context:OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
+    role: 'system',
+    content: 'You are a helpful chatbot'
+}]
 
-process.stdin.addListener('data', async function (input){
-    const userInput = input.toString().trim();
+async function createChatCompletion( ) {
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        messages: [{
-            role: 'system',
-            content: 'You are a helpful chatbot'
-        }, {
-            role: 'user',
-            content: userInput
-        }]
+        messages: context
+        })
+        const responseMessage = response.choices[0].message;
+        context.push({
+            role: 'assistant',
+            content: responseMessage.content
+        })
+        console.log(`${response.choices[0].message.role}: ${response.choices[0].message.content}`)
+    }
+process.stdin.addListener('data', async function (input){
+    const userInput = input.toString().trim();
+    context.push({
+        role: 'user',
+        content: userInput
     })
-    console.log(response.choices[0].message.content)
+    await createChatCompletion();
 })
