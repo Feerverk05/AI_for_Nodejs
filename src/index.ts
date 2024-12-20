c5ff73d23828631038c9023ea728b8ec30243f7b
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-20 14:28:08 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,15 @@ async function main(){
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages:[{
-            role: 'system',
-            content: `You respond like a cool bro and you respond in JSON format like this^ 
-            coolnessLevel: 1-10,
-            answer: your answer`
-        },{
             role: 'user',
-            content: 'How tall is mount Everest?'
-        }]
+            content: 'Іфн іщьуерштп сщщд'
+        }],
+        frequency_penalty: 1.5,
+        seed:555
     })
-    console.log(response.choices[0].message)
+    console.log(response.choices[0].message.content)
+    console.log(response.choices[1].message.content)
+
 }
 
 function encodePrompt(){
