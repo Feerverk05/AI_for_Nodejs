cb2b34b6e23d5edf1c633d0c73789dcf6476032d
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-21 14:44:56 +0200

    Update index.ts

--- chat_app/src/tools/index.ts
+++ chat_app/src/tools/index.ts
@@ -2,9 +2,18 @@ import OpenAI from "openai";
 
 const openAI = new OpenAI();
 
-function getTimeOfDay() {
-    return '5:45';
+function getTimeOfDay(){
+    return '5:45'
 }
+
+function getOrderStatus(orderId: string){
+    console.log(`Getting the status of order ${orderId}`)
+    const orderAsNumber = parseInt(orderId);
+    if (orderAsNumber % 2 == 0) {
+        return 'IN_PROGRESS'
+    }
+    return 'COMPLETED'
+} 
 
 async function callOpenAIWithTools() {
     const context: OpenAI.Chat.ChatCompletionMessageParam[] = [
@@ -16,45 +25,77 @@ async function callOpenAIWithTools() {
             role: 'user',
             content: 'What is the time of day?'
         }
-    ];
+    ]
+
+    
+
 
     const response = await openAI.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: context,
-        functions: [
+        tools: [
             {
+                type: 'function',
+                function: {
                 name: 'getTimeOfDay',
                 description: 'Get the time of day',
-                parameters: {}
+                }
+            },
+            {
+                type: 'function',
+                function: {
+                    name: 'getOrderStatus',
+                    description: 'Returns the status of an order',
+                    parameters: {
+                        type: 'object',
+                        properties: {
+                            prderId: {
+                                type: 'string',
+                                description: 'The id of the order to get the status of'
+                            }
+                        },
+                        required: ['orderId']
+                    }
+
+                    
+                }
             }
         ],
-        function_call: { name: 'getTimeOfDay' } 
+        tool_choice: 'auto'
     });
 
-    const functionCall = response.choices[0].message?.function_call;
+    const willInvokeFunction = response.choices[0].finish_reason == 'tool_calls'
+    const toolCall = response.choices[0].message.tool_calls![0]
 
-    if (functionCall?.name === 'getTimeOfDay') {
-        const toolResponse = getTimeOfDay();
-
-        context.push({
-            role: 'assistant',
-            content: null,
-            function_call: functionCall
-        });
-
-        context.push({
-            role: 'function',
-            name: 'getTimeOfDay',
-            content: toolResponse
-        });
+    if (willInvokeFunction) {
+        const toolName = toolCall.function.name
+        if(toolName == 'getTimeOfDay'){
+            const toolResponse = getTimeOfDay();
+            context.push(response.choices[0].message);
+            context.push({
+                role: 'tool',
+                content: toolResponse,
+                tool_call_id: toolCall.id
+            })
+        }
+        if(toolName == 'getOrderStatus'){
+            const rawArgument = toolCall.function.arguments;
+            const parsedArguments = JSON.parse(rawArgument);
+            const toolResponse = getOrderStatus(parsedArguments.orderId);
+            context.push(response.choices[0].message);
+            context.push({
+                role: 'tool',
+                content: toolResponse,
+                tool_call_id: toolCall.id
+            })
+        }
+    
     }
 
     const secondResponse = await openAI.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: context
-    });
-
-    console.log(secondResponse.choices[0].message?.content);
+    })
+    console.log(secondResponse.choices[0].message.content)
 }
-
-callOpenAIWithTools();
+callOpenAIWithTools(); 
