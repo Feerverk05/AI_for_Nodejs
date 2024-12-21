2fb8c6f18411bcc2cc35b04314dab326a9bff16e
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-21 15:41:05 +0200

    Update images.ts

--- src/others/images.ts
+++ src/others/images.ts
@@ -31,4 +31,19 @@ async function generateFreeLocalImage() {
     }
 }
 
-generateFreeLocalImage();
+async function generateAdvancedImage() {
+    const response = await openai.images.generate({
+        prompt: 'Photo of a city at night with skyscrapers',
+        model: 'dall-e-3',
+        quality: 'hd',
+        size: '1024x1024',
+        response_format: 'b64_json'
+    });
+    const rawImage = response.data[0].b64_json;
+    if(rawImage){
+        writeFileSync('city.png', Buffer.from(rawImage, 'base64'))
+    }
+}
+
+
+generateAdvancedImage();
