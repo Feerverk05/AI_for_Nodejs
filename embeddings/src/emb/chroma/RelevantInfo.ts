ad239228242f0e31b2905a896c9f691533cb3a7b
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-22 01:50:11 +0200

    Update RelevantInfo.ts

--- embeddings/src/emb/chroma/RelevantInfo.ts
+++ embeddings/src/emb/chroma/RelevantInfo.ts
@@ -1,1 +1,18 @@
+import { ChromaClient, OpenAIEmbeddingFunction } from "chromadb";
+import OpenAI from "openai";
+const chroma = new ChromaClient({path: "http://localhost:8000"});
 
+const studentInfo = ``;
+
+const clubInfo = ``;
+
+const embeddingFunction: OpenAIEmbeddingFunction = new OpenAIEmbeddingFunction({
+    openai_api_key: process.env.OPENAI_API_KEY!,
+    openai_model: 'text-embedding-3-small'
+})
+
+const collectionName = "personal-infos";
+
+async function createCollection() {
+    await chroma.createCollection({name:collectionName});
+}
