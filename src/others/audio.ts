5bbf01a3b2b28bf2e4c9f8d8e28ed714fb88308d
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-21 16:10:35 +0200

    Update audio.ts

--- src/others/audio.ts
+++ src/others/audio.ts
@@ -11,4 +11,25 @@ async function createTranscription(){
     });
     console.log(response);
 }
-createTranscription()
+
+async function translate(){
+    const response = await openai.audio.translations.create({
+        file: createReadStream('AudioSample.m4a'),
+        model: 'whisper-1',
+    });
+    console.log(response);
+}
+
+async function textToSpeech(){
+    const sampleText = 'Ukraine is a very beautiful country'
+    const response = await openai.audio.speech.create({
+        input: sampleText,
+        voice: 'alloy',
+        model: 'tts-1',
+        response_format: 'mp3'
+    });
+    const buffer = Buffer.from(await response.arrayBuffer());
+    writeFileSync('Ukraine.mp3', buffer)
+}
+
+textToSpeech();
