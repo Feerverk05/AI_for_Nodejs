b15415894690d342e05f4d3020d512349a8ae415
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-22 14:49:56 +0200

    Update Model.ts

--- langchain/src/Model.ts
+++ langchain/src/Model.ts
@@ -4,13 +4,20 @@ const model = new ChatOpenAI({
     modelName: 'gpt-3.5-turbo',
     temperature: 0.8,
     maxTokens: 700,
-    verbose: true
+    //verbose: true
 })
 
 async function main() {
-    const responsel = await model.invoke(
+    //const responsel = await model.invoke(
+    //    'Give me 4 good books to read'
+    //);
+    //console.log(responsel.content)
+
+    const response2 = await model.batch([
+        'Hello',
         'Give me 4 good books to read'
-    )
+    ]);
+    console.log(response2)
 }
 
 main()
