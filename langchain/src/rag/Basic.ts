281967a469ca8544a528dc32275093a2271d43a9
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-22 21:59:46 +0200

    Add files via upload

---
+++ langchain/src/rag/Basic.ts
@@ -0,0 +1,47 @@
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { Document } from '@langchain/core/documents';
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+const model = new ChatOpenAI({
+    modelName: 'gpt-3.5-turbo',
+    temperature: 0.7,
+});
+
+const myData = [
+    "My name Kateryna",
+    "My name Yuliia",
+    "My favorite food is sushi",
+    "My favorite food is pizza"
+];
+
+const question = "What are my favorite foods";
+
+async function main() {
+    const vectorStore = new MemoryVectorStore(new OpenAIEmbeddings());
+    
+    await vectorStore.addDocuments(
+        myData.map(content => new Document({ pageContent: content }))
+    );
+
+    const retriever = vectorStore.asRetriever({ k: 2 });
+
+    const results = await retriever.getRelevantDocuments(question);
+    const resultDocs = results.map(result => result.pageContent);
+
+    const template = ChatPromptTemplate.fromMessages([
+        ['system', 'List the favorite foods based on the following data: {context}'],
+        ['human', '{input}']
+    ]);
+
+    const chain = template.pipe(model);
+
+    const response = await chain.invoke({
+        input: question,
+        context: resultDocs.join(', ')
+    });
+
+    console.log(response.content);
+}
+
+main();
