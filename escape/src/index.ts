fe4afeae8b096be2f94e0a3c253053c00978a8c7
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-23 21:48:21 +0200

    Update index.ts

--- escape/src/index.ts
+++ escape/src/index.ts
@@ -19,4 +19,30 @@ async function translate(){
     });
     console.log(result)
 }
-translate()
+
+async function translate2(){
+    const result = await inference.translation({
+        model: 'facebook/nllb-200-distilled-600M',
+        inputs: 'How is the weather in Paris',
+        //@ts-ignore
+        parameters: {
+            src_lang: 'eng_Latn',
+            tgt_lang: 'spaa_Latn'
+        }
+    });
+    console.log(result)
+}
+
+async function answearQuestion() {
+    const result = await inference.questionAnswering({
+        inputs: {
+            context: 'The quick brown fox jumps over the lazy dog',
+            //question: 'What color is the fox'
+            //question: 'Is dog lazy?'
+            question: 'What is the meaning of life?'
+
+        }
+    })
+    console.log(result);
+}
+answearQuestion()
